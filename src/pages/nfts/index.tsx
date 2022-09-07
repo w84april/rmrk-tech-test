70cbc9d882015a2aab2bfd25a9ff386862878942
w84april <donyaschefer@gmail.com>
2022-09-07 16:11:44 +0300

    Add src folder

--- src/pages/nfts/index.tsx
+++ src/pages/nfts/index.tsx
@@ -1,6 +1,6 @@
 import { Flex } from '@chakra-ui/react';
 import type { NextPage } from 'next';
-import Filter from './components/Filter';
+import Filter from '../../components/Filter';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
